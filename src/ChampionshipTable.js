5019ce9e76f566516654c660b9802cd35aa8647a
Ben Madley <benmadley@Sebastien-laptop.local>
2021-10-28 13:15:03 +0100

    Extract out functions.

--- src/ChampionshipTable.js
+++ src/ChampionshipTable.js
@@ -1,32 +1,11 @@
 function ChampionshipTable(props) {
   
-  function calculateDriverScore(allPositions) {
-    return props.totaler.getScoreForSeason(props.scorer, allPositions);
-  }
-
   const seasonResults = props.results;
 
-  const seasonLength = seasonResults[Object.keys(seasonResults)[0]].length;
-  let header = ['Driver'];
-  for (let i = 0; i < seasonLength; i++) {
-    header.push(i + 1);
-  }
-  header.push('Points');
-
-  header = header.map((cell) => <th key={cell}>{cell}</th>)
-
-
-  let driverRestultsAndTotals = {};
-  for (const driver in seasonResults) {
-    const results = seasonResults[driver].slice();
-
-    driverRestultsAndTotals[driver] = {positions:results, total: calculateDriverScore(results)}
-  }
-
-  const driverOrder = props.orderer.getOrder(driverRestultsAndTotals)
-
-  let table = driverOrder.map((driver) => [driver].concat(driverRestultsAndTotals[driver].positions, [driverRestultsAndTotals[driver].total]));
-
+  let header = createHeader();
+  let table = createBody();
+  
+  header = header.map((cell) => <th key={cell}>{cell}</th>);
   table = table.map((row, i) => <tr key={row[0]}>{row.map((cell, i) => <td key={i}>{cell}</td>)}</tr>)
 
   return (
@@ -39,6 +18,40 @@ function ChampionshipTable(props) {
       </tbody>
     </table>
   );
+
+  function createHeader() {
+    const seasonLength = seasonResults[Object.keys(seasonResults)[0]].length;
+    let header = ['Driver'];
+    for (let i = 0; i < seasonLength; i++) {
+      header.push(i + 1);
+    }
+    header.push('Points');
+
+    return header;
+  }
+
+  function createBody() {
+    let driverRestultsAndTotals = {};
+    for (const driver in seasonResults) {
+      const results = seasonResults[driver].slice();
+
+      driverRestultsAndTotals[driver] = { 
+        positions: results, 
+        total: calculateDriverScore(results) 
+      };
+    }
+
+    const driverOrder = props.orderer.getOrder(driverRestultsAndTotals);
+
+    let table = driverOrder.map(
+      (driver) => [driver].concat(driverRestultsAndTotals[driver].positions, [driverRestultsAndTotals[driver].total])
+    );
+    return table;
+  }
+
+  function calculateDriverScore(allPositions) {
+    return props.totaler.getScoreForSeason(props.scorer, allPositions);
+  }
 }
 
 export default ChampionshipTable;
